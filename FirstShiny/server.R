caa0a6505b68b41e0e96abee638e0ff99a00976b
darobertson <demetria.a.robertson@gmail.com>
2014-06-23 16:52:00 -0700

    Update1

--- FirstShiny/server.R
+++ FirstShiny/server.R
@@ -7,20 +7,18 @@ shinyServer(function(input, output) {
   
 # Combine the selected variables into a new data frame
   IrisDataset <- reactive({
-           iris[iris$Species == input$species,]
-  selectedVariables <- ({
-           IrisDataset[, c(input$xcol, input$ycol)]
-           })
+           iris[iris$Species == input$species,c(input$xcol, input$ycol)]
+  })
+
 
 # Create output summary and plot
   output$summary <- renderPrint({
-    summary.data <- selectedVariables()
+    summary.data <- IrisDataset()
     summary(summary.data)
 })
 
   output$plot1 <- renderPlot({
              par(mar = c(5.1, 4.1, 0, 1))
-             plot(selectedVariables, main = c("Iris Regression for Species ", input$species))
+             plot(IrisDataset, main = c("Iris Regression for Species ", input$species))
            })
   })
-})
